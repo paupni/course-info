32e2c8416ad011aff78992b6580df8d34de1c25f
Paula Pniewska <pniewska.paula@gmail.com>
2026-05-27 22:42:10 +0200

    hello main commit

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,8 @@
+package com.pluralsight.courseinfo.cli;
+
+public class CourseRetriever {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, welcome to the Course Retriever!");
+    }
+}
