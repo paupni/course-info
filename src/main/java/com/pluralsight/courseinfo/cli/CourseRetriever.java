b3e091843e1b9bcd93a7ca4ea944124da09b4650
Paula Pniewska <pniewska.paula@gmail.com>
2026-05-27 22:49:26 +0200

    add main method with course ID command line argument

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -4,5 +4,20 @@ public class CourseRetriever {
 
     public static void main(String[] args) {
         System.out.println("Hello, welcome to the Course Retriever!");
+        if(args.length == 0) {
+            System.out.println("Please provide a course ID as an argument.");
+            return;
+        }
+
+        try {
+            retrieveCourse(args[0]);
+        } catch (Exception e) {
+            System.out.println("An error occurred while retrieving the course information: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void retrieveCourse(String courseId) {
+        System.out.println("Retrieving information for course ID: " + courseId);
     }
 }
